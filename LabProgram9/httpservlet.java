b59cb765d44b68785ecc40864566f7a2c46c4413
ADITYA SINGH <119720155+EchoSingh@users.noreply.github.com>
2024-07-24 23:25:52 +0530

    Update httpservlet.java

--- LabProgram9/httpservlet.java
+++ LabProgram9/httpservlet.java
@@ -1,52 +1,87 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
-import java.util.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
 
-public class DataAccess extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        String DB_URL = "jdbc:mysql://localhost/testdb";
-        String USER = "root";
-        String PASS = "root";
+public class DatabaseAccess extends HttpServlet {
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+   
+       String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+       String DB_URL = "jdbc:mysql://localhost/ise";
+
+       String USER = "root";
+       String PASS = "";
+
+       response.setContentType("text/html");
+       PrintWriter out = response.getWriter();
+       String title = "Database Result";
         
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        String title = "Database Request";
-        String docType = "<!DOCTYPE html public \"-//w3c//dtd html 4.01 transitional//en\">\n";
-        
-        out.println(docType + "<html>\n" + "<head><title>" + title + "</title></head>\n" + 
-                    "<body bgcolor=\"#f0f0f0\">\n" + 
-                    "<h1 align=\"center\">" + title + "</h1>\n");
-        
-        try {
-            Class.forName(JDBC_DRIVER);
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement();
-            String sql = "SELECT * FROM emp";
-            ResultSet rs = stmt.executeQuery(sql);
-            
-            out.println("<table border=1><tr><th>ID</th><th>Name</th><th>Age</th></tr>");
-            
-            while (rs.next()) {
-                int id = rs.getInt(1);
-                String name = rs.getString(2);
-                String age = rs.getString(3);
-                
-                out.println("<tr><td>" + id + "</td><td>" + name + "</td><td>" + age + "</td></tr>");
-            }
-            
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException se) {
-            out.println(se);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        out.println("</body></html>");
-    }
-}
+       String docType =
+         "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n";
+      
+       out.println(docType +
+         "<html>\n" +
+         "<head><title>" + title + "</title></head>\n" +
+         "<body bgcolor=\"#f0f0f0\">\n" +
+         "<h1 align=\"center\">" + title + "</h1>\n");
+
+       Connection conn = null;
+       Statement stmt = null;
+       try {
+         // Register JDBC driver
+         Class.forName(JDBC_DRIVER);
+
+         // Open a connection
+         conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+         // Execute SQL query
+         stmt = conn.createStatement();
+         String sql = "SELECT * FROM emp";
+         ResultSet rs = stmt.executeQuery(sql);
+
+         // Extract data from result set
+         out.println("<table border=1>");
+         out.println("<tr><th>ID</th><th>Name</th><th>Age</th></tr>");
+         while (rs.next()) {
+            //Retrieve by column name
+            int id = rs.getInt(1);
+            String name = rs.getString(2);
+            String age = rs.getString(3);
+
+            //Display values
+            out.println("<tr><td>" + id + "</td><td>" + name + "</td><td>" + age + "</td></tr>");
+         }
+         out.println("</table>");
+         out.println("</body></html>");
+
+         // Clean-up environment
+         rs.close();
+         stmt.close();
+         conn.close();
+      } catch (SQLException se) {
+         //Handle errors for JDBC
+         out.println(se);
+         se.printStackTrace();
+      } catch (Exception e) {
+         //Handle errors for Class.forName
+         e.printStackTrace();
+         out.println(e);
+      } finally {
+         // finally block used to close resources
+         try {
+            if (stmt != null) stmt.close();
+         } catch (SQLException se2) {
+         } // nothing we can do
+         try {
+            if (conn != null) conn.close();
+         } catch (SQLException se) {
+            se.printStackTrace();
+         } //end finally try
+      } //end try
+   } //end doGet
+} //end DatabaseAccess
